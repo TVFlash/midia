0c28ee653d5a1d925fa78e0e040f309b23537a32
lee1543 <lee1543@purdue.edu>
2016-10-03 12:32:57 -0400

    Login popover UI
    Added login UI

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 var app = angular.module('MidiaApp', []);
+
+$(function(){
+    $('#login').popover({       
+        placement: 'bottom',
+        html:true,
+        content:  $('#loginForm').html()
+    }).on('click', function(){
+      $('.btn-primary').click(function(){
+       
+        $.post('/',  {
+            username: $('#username').val(),
+            password: $('#password').val(),
+        }, function(r) {
+          
+        })
+      })
+  })
+})
