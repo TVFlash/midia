4edb56031a207395484511ad9851ea226a14b45c
Tim Vincent <vincent3@purdue.edu>
2016-12-08 18:30:27 -0500

    Added type to josn object

--- server.py
+++ server.py
@@ -65,7 +65,7 @@ class postObject:
 		self.sublabel = ''
 
 	def to_json(self):
-		return {"id": self.id, "source": self.source, "message": self.message, "time": self.time, "link": self.link, "picture": self.picture, "mainlabel": self.mainlabel, "sublabel": self.sublabel}
+		return {"id": self.id, "type": self.type, "source": self.source, "message": self.message, "time": self.time, "link": self.link, "picture": self.picture, "mainlabel": self.mainlabel, "sublabel": self.sublabel}
 
 connected_users = {}
 
